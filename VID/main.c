f93d5135f16c92fc854b295a7048d57ca9bf7464
IMaylatov <IMaylatov@gmail.com>
2016-08-31 21:31:31 +0300

    Новый проект

---
+++ VID/main.c
@@ -0,0 +1,18 @@
+/*
+ * VID.c
+ *
+ * Created: 31.08.2016 21:29:32
+ * Author : IMayl
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
