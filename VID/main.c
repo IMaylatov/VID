e5d64c598da6c64e60e5ab0d787e4de29d1d2220
IMaylatov <IMaylatov@gmail.com>
2017-02-01 22:14:29 +0300

    считаем угловую скорость. прерывания int0 int1, прерывание по таймеру. Двигатель крутиться, с рабочими кнопками, угловая скорость считается, но нигде не участвуется

--- VID/main.c
+++ VID/main.c
@@ -1,38 +1,94 @@
 // TC0_fast_PWM.c
 #define F_CPU 1000000UL
 #include <avr/io.h>
+#include <math.h>
 #include <util/delay.h>
+#include <avr/interrupt.h>
+
+unsigned char allTimePWM = 255;
+unsigned char timeU = 200;
+int maxRepeatCount = 500;
+int repeatCount = 500;
+char phaze = 0;
+long circle = 0;
+
+SIGNAL (TIMER0_COMPA_vect)
+{	
+	if((PINB & (1 << PINB0)) || (PINB & (1 << PINB1)) || (PINB & (1 << PINB2))){
+		PORTB = 0;
+		repeatCount--;
+		OCR0A = allTimePWM-timeU;
+		}else{
+		if(repeatCount==0){
+			repeatCount = maxRepeatCount;
+			phaze++;
+			if(phaze>2){
+				phaze=0;
+			}
+		}
+		switch (phaze){
+			case 0:
+			PORTB = (1 << PINB0);
+			break;
+			case 1:
+			PORTB = (1 << PINB2);
+			break;
+			case 2:
+			PORTB = (1 << PINB1);
+			break;
+		}
+		OCR0A = timeU;
+	}
+}
+
+SIGNAL (INT0_vect){
+	if(PIND & (1 << PIND4)){
+		maxRepeatCount+=10;
+		}else{
+		if(maxRepeatCount-10>0) {
+			maxRepeatCount-=10;
+		}
+	}
+}
+
+double period;
+double angleSpeed;
+double endPeriod;
+
+SIGNAL (INT1_vect){
+	circle++;
+	if (circle > 2){
+		endPeriod = TCNT1;
+		period = endPeriod / 10000;		// seconds
+		angleSpeed = 2 * M_PI / period;
+	}
+	TCNT1=0;
+}
+
+void InitInterupt(){
+	PCMSK |= (1<<PIND2);
+	MCUCR |= (0<<ISC00) | (1<<ISC01) | (0<<ISC10)| (1<<ISC11);
+	GIMSK |= (1<<INT0) | (1<<INT1);
+}
 
 int main(void)
 {
-	unsigned char PWM_val1 = 0;		// 8-bit PWM value
-	unsigned char PWM_val2 = 255;	// 8-bit PWM value
-	unsigned char up_dn = 0;		// up down count flag
+	DDRB |= (1 << PINB0) | (1 << PINB1) | (1 << PINB2);
+	DDRD |= (1 << PIND4) | (0 << PIND2) | (0 << PIND3);
 	
-	DDRB   |= (1 << PINB2);                   // PWM output on PB2 - OC0A
-	DDRB   |= (1 << PINB3);                   // PWM output on PB2 - OC0A
-	DDRD   |= (1 << PIND5);                   // PWM output on PD5 - OC0B
-	// fast PWM mode
-	TCCR0A = (1 << COM0A1) | (1 << COM0B1) | (1 << WGM01) | (1 << WGM00);
-	TCCR1A = (1 << COM0A1) | (1 << COM0B1) | (1 << WGM01) | (1 << WGM00);
-	TCCR0B = (1 << CS01);   // clock source = CLK/8, start PWM
+	InitInterupt();
+	
+	TCCR0A = 0x02;      // Clear Timer on Compare Match (CTC) mode
+	TIFR |= 0x01;       // clear interrupt flag
+	TIMSK = 0x01;       // TC0 compare match A interrupt enable
+	TCCR0B = (1 << CS00);      // clock source CLK, start timer
+	
+	TCCR1A = 0x00;             // CTC mode
+	TCCR1B = (1 << CS00);   // start timer
+	
+	sei();
 	
 	while(1)
 	{
-		if ((PWM_val1 == 255) || (PWM_val1 == 0)) {
-			up_dn ^= 0x01;      // toggle count direction flag
-		}
-		_delay_ms(5);
-		OCR0A  = PWM_val1;       // write new PWM value
-		OCR1A = PWM_val2;
-		OCR0B  = PWM_val2;
-		if (up_dn) {            // increment or decrement PWM value
-			PWM_val1++;
-			PWM_val2--;
-		}
-		else {
-			PWM_val1--;
-			PWM_val2++;
-		}
 	}
 }
