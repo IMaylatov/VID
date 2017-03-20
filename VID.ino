add4d532a1162e28b449350d73c0e47ca561e53e
IMaylatov <IMaylatov@gmail.com>
2017-03-20 22:23:01 +0300

    подключил вероятностное моделирование, корявый расчет параметров (только расчет, ничего не применяется)

--- VID.ino
+++ VID.ino
@@ -1,4 +1,5 @@
 #include <DueTimer.h>
+#include <math.h>
 
 const byte A_PHASE = 2;
 const byte B_PHASE = 1;
@@ -13,26 +14,42 @@ const byte M_PIN = A0;
 byte phase;
 int phasePeriod;
 
-int u;
+int Ud;
+double T;
 
-int M;
+double M;
 int valueMpin;
 
 double w;
 int firstRotorSensorTime, lastRotorSensorTime;
+
+const double Lm = 0.4472135955;
+const double Ls = 2;
+const double Lr = 0.1;
+const double Tr = 0.0995;
+const double Rr = 0.2;
+const double Rs = 0.1; 
+const double sigma = 0.7;
+const double tau = 0.000001;
+const double e = 0.000001;
+
+double psi;
+
+double I0[2];
+double u[2][12];
 
 void nextPhase(){  
   disableAllPhasePin();
   phase++;
   switch(phase){
     case A_PHASE:
-      analogWrite(A_PHASE_PIN, u);
+      analogWrite(A_PHASE_PIN, Ud);
       break;
     case B_PHASE:
-      analogWrite(B_PHASE_PIN, u);
+      analogWrite(B_PHASE_PIN, Ud);
       break;
     case C_PHASE:
-      analogWrite(C_PHASE_PIN, u);
+      analogWrite(C_PHASE_PIN, Ud);
       break;
     default:
       phase = -1;
@@ -50,9 +67,186 @@ void disableAllPhasePin(){
 
 void rotorSensorInterrupt(){
   lastRotorSensorTime = millis();
-  int rotorSensorPeriod = lastRotorSensorTime - firstRotorSensorTime;
-  Serial.println(rotorSensorPeriod);
+  double rotorSensorPeriodMilli = lastRotorSensorTime - firstRotorSensorTime;
+  double rotorSensorPeriod = rotorSensorPeriodMilli / 1000;
+//  w = 2 * M_PI / rotorSensorPeriod;
   firstRotorSensorTime = lastRotorSensorTime;
+
+  w = 10;
+  M = 0.001;
+  CalculateUdT();
+}
+
+void CalculateUdT()
+{
+  psi = GetEcsponentPsi();
+
+  double nu0[2][2];
+  nu0[0][0] = psi;
+  nu0[0][1] = psi + e;
+  
+  double v = -tau * GetDifPsi(psi) / e;
+  v = v*(nu0[0][1] - nu0[0][0]);
+  
+  nu0[1][0] = v + nu0[0][0];
+  nu0[1][1] = v + nu0[0][1];  
+
+  I0[0] = min(nu0[0][0],min(nu0[0][1],min(nu0[1][0],nu0[1][1])));
+  I0[1] = max(nu0[0][0],max(nu0[0][1],max(nu0[1][0],nu0[1][1])));
+
+  u[0][5] = nu0[1][0];
+  u[0][6] = nu0[1][1];
+  T = tau;
+
+  SearchLastStep();
+  
+  double UdTmp = GetUd();
+  Serial.println(UdTmp, 20);
+}
+
+double GetEcsponentPsi()
+{
+  double psi1 = sigma * Lr * Ls * Tr * w + Lm * Lm * Tr * w - sigma * Lr * Ls - Lm * Lm;
+  double psi2 = sqrt(-6 * Rr * psi1 * M * Rs * Tr) * Lr;
+  return psi2 / (3 * Rr * psi1);
+}
+
+double GetDifPsi(double p)
+{
+  double psi1 = -(Lm * Lm + Lr * Ls * Tr * sigma * (2 + Lm * w)) / (2 * Lr * Ls * sigma * Tr);
+  double psi2 = p * (psi1 - Lr * M / (3 * p * p * Rr * Tr));
+
+  double psi3 = Lm * Lm / (Lr * Ls * sigma * Tr) - w + 2 / 3 * Lr * M / (Lm * p * p * Rr * Tr);
+  double psi4 = (1 / 4.0 * psi3 * psi3 + Lm * Lm * w / (Lr * Ls * sigma * Tr)) * (p * p);
+
+  double psi5 = 2 / 3.0 * (Lr * M) / Rr * (Rs / (Ls * sigma * Tr) + 1 / (Tr * Tr));
+
+  double psi6 = sqrt(psi4 + psi5);
+
+  return psi2 + psi6;
+}
+
+void SearchLastStep()
+{
+  bool doNextStepByTime;
+  do
+  {
+    doNextStepByTime = false;
+
+    int i = 5;
+    double probability = GetCurrentProbability(i, GetDifPsi(psi));
+    double sideProbability;
+    if (probability > 0)
+    {
+      i = 6;
+      do
+      {
+          sideProbability = GetUpProbability(i, GetDifPsi(psi + e * (i - 5)));
+          i++;
+      } while (i < 11 && sideProbability > 0);
+      if (i < 11)
+      {
+          i = 5;
+          do
+          {
+              sideProbability = GetDownProbability(i, GetDifPsi(psi + e * (i - 5)));
+              i--;
+          } while (i > 0 && sideProbability > 0);
+          if (i > 0)
+          {
+              u[0][5] = u[1][5];
+              u[0][6] = u[1][6];
+              T += tau;
+              doNextStepByTime = true;
+          }
+      }
+    }
+  }while (doNextStepByTime);
+}
+
+double GetUpProbability(int i, double p)
+{
+  double v = tau * p / e;
+
+  u[1][i + 1] = (u[0][i] - u[1][i]*(1 - v))/v;
+  u[0][i + 1] = (u[0][i] - u[1][i]) / v + u[0][i];
+
+  double maxU = max(u[0][i], max(u[1][i], max(u[0][i + 1], u[1][i + 1])));
+  double minU = min(u[0][i], min(u[1][i], min(u[0][i + 1], u[1][i + 1])));
+
+  return GetProbability(minU, maxU);
+}
+
+double GetDownProbability(int i, double p)
+{
+  double v = tau * p / e;
+
+  u[0][i - 1] = (u[1][i] - u[0][i]*(1 - v))/v;
+  u[1][i - 1] = (u[1][i]*(1 + v) - u[0][i])/v;
+
+  double maxU = max(u[0][i], max(u[1][i], max(u[0][i - 1], u[1][i - 1])));
+  double minU = min(u[0][i], min(u[1][i], min(u[0][i - 1], u[1][i - 1])));
+
+  return GetProbability(minU, maxU);
+}
+
+double GetCurrentProbability(int i, double p)
+{
+  double v = tau * p / e;
+
+  u[1][i] = v * (u[0][i] - u[0][i + 1]) + u[0][i];
+  u[1][i + 1] = v * (u[0][i] - u[0][i + 1]) + u[0][i + 1];
+
+  double maxU = max(u[0][i], max(u[1][i], max(u[0][i + 1], u[1][i + 1])));
+  double minU = min(u[0][i], min(u[1][i], min(u[0][i + 1], u[1][i + 1])));
+
+  return GetProbability(minU, maxU);
+}
+
+double GetProbability(double minU, double maxU)
+{
+  if (I0[1] <= minU || I0[0] >= maxU) return 0;
+  if (I0[1] <= maxU && I0[0] >= minU) return 1;
+  if (I0[1] >= maxU && I0[0] >= minU) return (maxU - I0[0]) / (I0[1] - I0[0]);
+
+  if (I0[1] <= maxU && I0[0] <= minU) return (I0[1] - minU) / (I0[1] - I0[0]);
+  if (I0[1] >= maxU && minU >= I0[0]) return (maxU - minU) / (I0[1] - I0[0]);
+
+  return 0;
+}
+
+double GetId()
+{
+  double id1 = -Lm * psi / (2 * Lr * Ls * sigma);
+  double id2 = Tr * w * psi / 2.0;
+  double id3 = -Lr / (3 * Lm * psi) * M / Rr;
+  double id4 = Lm * psi / (Lr * Ls * sigma) - Tr * w * psi + 2 / 3 * Lr * M / (Lm * psi * Rr);
+  double id5 = 0.25 * id4 * id4;
+  double id6 = 2 * Lr * M / (3 * Lm * Lm * Rr) * (Rs * Tr / (Ls * sigma) + 1);
+  double id7 = Tr * w * psi * psi / (Lr * Ls * sigma);
+
+  return id1 + id2 + id3 + sqrt(id5 + id6 + id7);
+}
+
+double GetUd()
+{
+  double Id = GetId();
+
+  double u1 = -Lm / (2 * Lr * Ls * sigma);
+  double u2 = Tr * w / 2.0;
+  double u3 = Lr * M / (3 * Lm * psi * psi * Rr);
+  double u4 = Lm * psi / (Lr * Ls * sigma) - Tr * w * psi + 2 * Lr * M / (3 * Lm * psi * Rr);
+  double u5 = Lm / (Ls * Lr * sigma) - Tr * w - 2 * Lr * M / (3 * Lm * psi * psi * Rr);
+  double u6 = 4 * Tr * w * psi / (Lr * Ls * sigma);
+  double u7 = u4 * u5 + u6;
+  double u8 = 8 * Lr * M / (3 * Lm * Lm * Rr) * (Rs * Tr / (Ls * sigma) + 1);
+  double u9 = 4 * Tr * w * psi * psi / (Lr * Ls * sigma);
+  double u10 = 2 * Tr * sqrt(u4 * u4 + u8 + u9);
+  double u11 = sigma * Ls * (u1 + u2 + u3 + u7 / u10) * (-psi + Lm * Id) + Rs * Id;
+  double u12 = -2 / 3.0 * sigma * Lr * Ls * M / Rr * (w / (Lm * psi) - Id / (Tr * psi * psi));
+  double u13 = -Lm / (Lr * Tr) * (psi - Lm * Id);
+
+  return u11 + u12 + u13;
 }
 
 void setup() {  
@@ -63,7 +257,7 @@ void setup() {
   pinMode(C_PHASE_PIN, OUTPUT);
 
   phase = A_PHASE;
-  u = 255;
+  Ud = 255;
   phasePeriod = 50000;
   Timer4.attachInterrupt(nextPhase).start(phasePeriod);
 
@@ -72,6 +266,5 @@ void setup() {
 }
 
 void loop() {
-  valueMpin = analogRead(M_PIN);
-  phasePeriod = map(valueMpin, 0, 1023, 500, 50000);
+  int a = analogRead(M_PIN);
 }
